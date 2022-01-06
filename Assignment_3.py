979b441256df159aec85371012fd3820d9f67f0c
Shahil Patel <83348636+shahilpatel-3602@users.noreply.github.com>
2022-01-06 23:31:27 +0530

    Update Assignment_3.py
    Commit to negation function and tautology function.

--- Assignment_3.py
+++ Assignment_3.py
@@ -12,18 +12,57 @@ class Stage:
         self.previous_storage = previous_storage
 
 
+def negative(literal):
+
+    if literal == 'a':
+        return '~a'
+
+    elif literal == '~a':
+        return 'a'
+
+    elif literal == 'b':
+        return '~b'
+
+    elif literal == '~b':
+        return 'b'
+
+    elif literal == 'c':
+        return '~c'
+
+    elif literal == '~c':
+        return 'c'
+
+    elif literal == '~d':
+        return 'd'
+
+    elif literal == 'd':
+        return '~d'
 
 
+def tautology(clause):
+
+    for literal in clause:
+
+        if negative(literal) in clause:     #if negation of any literal is present in clause then it is tautology
+            return True
+
+        return False
 
 
 #Main Function / driver code
 
 n = 4     #here n is no. of literals given in question
 k = 5     #here k is no. of clauses given in question
-possibilities =['a','~a','b','~b','c','~c','d','~d']
-combination =[]
 
+possibilities = ['a','~a','b','~b','c','~c','d','~d']
+combination = []
+formula = []
 for i in range(k):
     reject = 1
     while(reject):
-        random_combo=[random.choice(possibilities) for j in range(3)]
+        random_combo=[random.choice(possibilities) for j in range(3)]  #generate random clause
+
+    #     if (not tautology(random_combo)) and (not duplicate(random_combo) and (len(set(random_combo)) == len(random_combo))):   # check if it is allowed
+    #         rejected = False
+            
+    # formula.append(random_combo)
